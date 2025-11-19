b9b0efbb3a73cd300d63a868eec7f4d27e4bcff7
Yusuf Alshalabi <yusuf.oftt@gmail.com>
2025-11-19 03:51:35 +0200

    Feat : Add Transactions menu structure and navigation

--- BankSystem/BankSystem.cpp
+++ BankSystem/BankSystem.cpp
@@ -20,6 +20,7 @@ struct strClient {
 };
 
 void showMainMenuScreen();
+void ShowTransactionsMenue();
 //Pause until user presses a key
 void customPause() {
 	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -32,6 +33,11 @@ void clearScreen() {
 #else
 	system("clear");
 #endif
+}
+void goBackToTransactionsMenue() {
+	cout << "\n\nPress any key to go back to Main Menu...";
+	customPause();
+	ShowTransactionsMenue();
 }
 void goBackToMainMenu() {
 	cout << "\n\nPress any key to go back to Main Menu...";
@@ -164,8 +170,17 @@ enum MainMenuOption {
 	DeleteClient = 3,
 	UpdateClient = 4,
 	FindClient = 5,
-	Exit = 6
+	Transactions = 6,
+	Exit = 7
 };
+
+enum TransactionsMenueOptions { 
+	Deposit = 1, 
+	Withdraw = 2,
+	ShowTotalBalance = 3,
+	ShowMainMenue = 4 
+};
+
 
 void showScreenHeader(const string& title) {
 	clearScreen();
@@ -361,20 +376,92 @@ void showExitClient() {
 
 	showScreenHeader("Program Ends :-)");
 }
-// Read user's choice from main menu (1 to 6)
-MainMenuOption readMainMenuOption() {
-	showMainMenuScreen();
+
+void PerfromTranactionsMenueOption(TransactionsMenueOptions TransactionMenueOption)
+{
+	switch (TransactionMenueOption)
+	{
+	case TransactionsMenueOptions::Deposit:
+	{
+		clearScreen();
+		//ShowDepositScreen();
+		goBackToTransactionsMenue();
+		break;
+	}
+
+	case TransactionsMenueOptions::Withdraw:
+	{
+		clearScreen();
+		//ShowWithDrawScreen();
+		goBackToTransactionsMenue();
+		break;
+	}
+
+
+	case TransactionsMenueOptions::ShowTotalBalance:
+	{
+		clearScreen();
+		//ShowTotalBalancesScreen();
+		goBackToTransactionsMenue();
+		break;
+	}
+
+
+	case TransactionsMenueOptions::ShowMainMenue:
+	{
+		showMainMenuScreen();
+		break;
+	}
+	}
+
+}
+
+
+TransactionsMenueOptions readTransactionsMenueOption() {
+	ShowTransactionsMenue();
 	int choice;
 	do {
-		cout << "Choose what do you want to do? [1 to 6]? ";
+		cout << "Choose what do you want to do? [1 to 4]? ";
 		cin >> choice;
 		while (cin.fail()) {
 			cin.clear();
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << "Invalid Number, Enter a valid one: [1 to 6]? " << endl;
+			cout << "Invalid Number, Enter a valid one: [1 to 4]? " << endl;
 			cin >> choice;
 		}
-	} while ((choice < 1) || (choice > 6));
+	} while ((choice < 1) || (choice > 4));
+
+	return (TransactionsMenueOptions)choice;
+
+}
+
+void ShowTransactionsMenue(){
+	system("cls");
+	cout << "===========================================\n";
+	cout << "\t\tTransactions Menue Screen\n";
+	cout << "===========================================\n";
+	cout << "\t[1] Deposit.\n";
+	cout << "\t[2] Withdraw.\n";
+	cout << "\t[3] Total Balances.\n";
+	cout << "\t[4] Main Menue.\n";
+	cout << "===========================================\n";
+	PerfromTranactionsMenueOption((TransactionsMenueOptions)readTransactionsMenueOption());
+}
+
+// Read user's choice from main menu (1 to 7)
+MainMenuOption readMainMenuOption() {
+	showMainMenuScreen();
+	int choice;
+	do {
+		cout << "Choose what do you want to do? [1 to 7]? ";
+		cin >> choice;
+		while (cin.fail()) {
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cout << "Invalid Number, Enter a valid one: [1 to 7]? " << endl;
+			cin >> choice;
+		}
+	} while ((choice < 1) || (choice > 7));
 
 	return (MainMenuOption)choice;
 
@@ -402,6 +489,9 @@ void performMainMenuOption(MainMenuOption MainMenuOption, vector<strClient>& vCl
 	case MainMenuOption::FindClient:
 		ShowFindClientScreen(vClients);
 		goBackToMainMenu();
+		break;
+	case MainMenuOption::Transactions:
+		ShowTransactionsMenue();
 		break;
 	case MainMenuOption::Exit:
 		showExitClient();
@@ -436,7 +526,8 @@ void showMainMenuScreen() {
 	cout << "\t[3] Delete Client.\n";
 	cout << "\t[4] Update Client.\n";
 	cout << "\t[5] Find Client.\n";
-	cout << "\t[6] Exit.\n";
+	cout << "\t[6] Transactions.\n";
+	cout << "\t[7] Exit.\n";
 	cout << "===========================================\n";
 
 }
