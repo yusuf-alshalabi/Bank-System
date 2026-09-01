f9332a7521b2e8a0096b4b9185b747176f5fb45d
Yusuf Alshalabi <yusuf.alshalabi.work@gmail.com>
2026-09-01 18:25:23 +0300

    feat(core): add Deposit and Withdraw methods to BankClient

--- BankSystem/Core/BankClient.h
+++ BankSystem/Core/BankClient.h
@@ -315,4 +315,18 @@ public:
 		return TotalBalances;
 
 	}
+
+
+	void Deposit(double Amount)
+	{
+		_AccountBalance += Amount;
+		Save();
+	}
+
+	void Withdraw(double Amount)
+	{
+		_AccountBalance -= Amount;
+		Save();
+	}
+
 };
