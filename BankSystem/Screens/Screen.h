2eece2a1daf5a53cad4f9dafa50b08199cb9cc20
Yusuf Alshalabi <yusuf.alshalabi.work@gmail.com>
2026-09-04 15:33:29 +0300

    style(ui): display current user and active date in base screen header

--- BankSystem/Screens/Screen.h
+++ BankSystem/Screens/Screen.h
@@ -2,19 +2,23 @@
 #include <iostream>
 #include <string>
 #include"../Core/User.h"
+#include "../../Libs/Cpp-Library-Collection/Lib/Date.h"
 #include "Global.h"
 class Screen
 {
 protected:
     static void _DrawScreenHeader(std::string Title, std::string SubTitle = "")
     {
-        std::cout << "\t\t\t\t\t===========================================\n";
-        std::cout << "\t\t\t\t\t  " << Title << "\n";
+        std::cout << "\t\t\t\t\t______________________________________";
+        std::cout << "\n\n\t\t\t\t\t  " << Title;
         if (SubTitle != "")
         {
-            std::cout << "\t\t\t\t\t  " << SubTitle << "\n";
+            std::cout << "\n\t\t\t\t\t  " << SubTitle;
         }
-        std::cout << "\t\t\t\t\t===========================================\n\n";
+        std::cout << "\n\t\t\t\t\t______________________________________\n";
+        std::cout << "\n\t\t\t\t\tUser: " << CurrentUser.UserName << "\n";
+        std::cout << "\t\t\t\t\tDate: " << Core::Date::ToString(Core::Date())
+            << "\n\n";
     }
 
     static bool CheckAccessRights(User::enPermissions Permission)
