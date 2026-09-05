2e0ee98633bbc7d4d63f80659f1d9de9fd3383fb
Yusuf Alshalabi <yusuf.alshalabi.work@gmail.com>
2026-09-05 21:55:19 +0300

    feat(ui): integrate TransferScreen into Transactions menu
    - Add TransferScreen header file to project files and solution filters.
    - Expand TransactionsScreen menu options (1 to 5) to include Transfer option.
    - Wire up TransferScreen execution logic within menu handler.

--- BankSystem/Screens/TransactionsScreen/TransactionsScreen.h
+++ BankSystem/Screens/TransactionsScreen/TransactionsScreen.h
@@ -8,6 +8,7 @@
 #include "DepositScreen.h"
 #include "WithdrawScreen.h"
 #include "TotalBalancesScreen.h"
+#include "TransferScreen.h"
 
 using namespace std;
 
@@ -18,13 +19,13 @@ class TransactionsScreen :protected Screen
 private:
     enum enTransactionsMenueOptions {
         eDeposit = 1, eWithdraw = 2,
-        eShowTotalBalance = 3, eShowMainMenue = 4
+        eShowTotalBalance = 3, eTransfer = 4, eShowMainMenue = 5
     };
 
     static short ReadTransactionsMenueOption()
     {
-        cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 4]? ";
-        short Choice = Core::InputValidate::ReadNumberBetween<short>(1, 4, "Enter Number between 1 to 4? ");
+        cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 5]? ";
+        short Choice = Core::InputValidate::ReadNumberBetween<short>(1, 5, "Enter Number between 1 to 5? ");
         return Choice;
     }
 
@@ -47,6 +48,12 @@ private:
        // cout << "\n Balances Screen will be here.\n";
         		TotalBalancesScreen::ShowTotalBalances();
     }
+
+	static void _ShowTransferScreen()
+	{
+		// cout << "\n Transfer Screen will be here.\n";
+		TransferScreen::ShowTransferScreen();
+	}
 
     static void _GoBackToTransactionsMenue()
     {
@@ -84,6 +91,13 @@ private:
             break;
         }
 
+		case enTransactionsMenueOptions::eTransfer:
+		{
+			system("cls");
+			_ShowTransferScreen();
+			_GoBackToTransactionsMenue();
+			break;
+		}
 
         case enTransactionsMenueOptions::eShowMainMenue:
         {
@@ -115,7 +129,8 @@ public:
         cout << setw(37) << left << "" << "\t[1] Deposit.\n";
         cout << setw(37) << left << "" << "\t[2] Withdraw.\n";
         cout << setw(37) << left << "" << "\t[3] Total Balances.\n";
-        cout << setw(37) << left << "" << "\t[4] Main Menue.\n";
+		cout << setw(37) << left << "" << "\t[4] Transfer.\n";
+        cout << setw(37) << left << "" << "\t[5] Main Menue.\n";
         cout << setw(37) << left << "" << "===========================================\n";
 
         _PerformTransactionsMenueOption((enTransactionsMenueOptions)ReadTransactionsMenueOption());
---
+++ BankSystem/Screens/TransactionsScreen/TransferScreen.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <iostream>
+#include "../Screen.h"
+#include "../../Core/Person.h"
+#include "../../Core/BankClient.h"
+#include "../../../Libs/Cpp-Library-Collection/Lib/InputValidate.h"
+
+class TransferScreen : protected Screen
+{
+
+private:
+
+	static void _PrintClient(const BankClient& Client)
+	{
+		std::cout << "\nClient Card:";
+		std::cout << "\n___________________";
+		std::cout << "\nFull Name   : " << Client.FullName();
+		std::cout << "\nAcc. Number : " << Client.GetAccountNumber();
+		std::cout << "\nBalance     : " << Client.GetAccountBalance();
+		std::cout << "\n___________________\n";
+	}
+
+	static string _ReadAccountNumber(const std::string& Prompt)
+	{
+		std::string AccountNumber = Core::InputValidate::ReadString(Prompt);
+		while (!BankClient::IsClientExist(AccountNumber))
+		{
+			std::cout << "\nClient with [" << AccountNumber << "] does not exist.\n";
+			AccountNumber = Core::InputValidate::ReadString(Prompt);
+		}
+		return AccountNumber;
+	}
+
+public:
+
+	static void ShowTransferScreen()
+	{
+		_DrawScreenHeader("\t   Transfer Screen");
+
+		BankClient SourceClient = BankClient::Find(_ReadAccountNumber("\nPlease enter Account Number to transfer from: "));
+		_PrintClient(SourceClient);
+
+		BankClient DestinationClient = BankClient::Find(_ReadAccountNumber("\nPlease enter Account Number to transfer to: "));
+		_PrintClient(DestinationClient);
+
+		double Amount = Core::InputValidate::ReadNumber<double>("\nEnter transfer amount? ");
+
+		while (Amount <= 0)
+		{
+			std::cout << "\nTransfer amount must be greater than zero.\n";
+			Amount = Core::InputValidate::ReadNumber<double>("\nEnter transfer amount? ");
+		}
+
+		while (SourceClient.GetAccountBalance() < Amount)
+		{
+			std::cout << "\nAmount Exceeds the available Balance,";
+			Amount = Core::InputValidate::ReadNumber<double>("\nEnter another amount : ");
+		}
+
+
+		if (Core::InputValidate::ReadYesNoOption("\nAre you sure you want to perform this transaction? "))
+		{
+			SourceClient.Transfer(Amount, DestinationClient);
+			std::cout << "\nAmount Transferred Successfully.\n";
+			_PrintClient(SourceClient);
+			_PrintClient(DestinationClient);
+		}
+		else
+		{
+			std::cout << "\nOperation was cancelled.\n";
+		}
+	}
+};
+
